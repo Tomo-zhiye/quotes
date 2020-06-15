2cca1410fbf89b079178ed2afe3c56d94be142fd
Tomo-zhiye <oishiimono051128@gmail.com>
2020-06-16 07:33:52 +0800

    Extracting Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quotes/quote.dart';
 import 'package:quotes/quote_model.dart';
+
+import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(
       home: QuoteList(),
     ));
 
 class QuoteList extends StatelessWidget {
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.quote,
-              style: TextStyle(
-                fontSize: 13.0,
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Center(
       child: ChangeNotifierProvider<QuoteModel>(
-        create: (_) => QuoteModel(),
+        create: (_) => QuoteModel()..fetchQuotes(),
         child: Scaffold(
           backgroundColor: Colors.grey[200],
           appBar: AppBar(
-            title: Text('Great Quotes'),
+            title: Text('Awesome Quotes'),
             centerTitle: true,
             backgroundColor: Colors.redAccent,
           ),
           body: Consumer<QuoteModel>(builder: (context, model, child) {
             return Column(
-                children:
-                    model.quotes.map((quote) => quoteTemplate(quote)).toList());
+                children: model.quotes
+                    .map((quote) => QuoteCard(quote: quote))
+                    .toList());
           }),
         ),
       ),
---
+++ lib/quote_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:quotes/quote.dart';
+
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+  QuoteCard({this.quote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.quote,
+              style: TextStyle(
+                fontSize: 13.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
